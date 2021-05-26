8a39d7d59f5adc6987a266edfb7e3e5286490531
shanthibabu <73182461+shanthi1999@users.noreply.github.com>
2021-05-26 17:06:58 +0530

    Update push.js

--- push.js
+++ push.js
@@ -7,12 +7,6 @@ console.log(vapIdKeys)
 
 push.setVapidDetails('mailto:shanthibabu1999@gmail.com',vapIdKeys.publicKey,vapIdKeys.privateKey);
 
-let sub = {
-    endpoint:"https://fcm.googleapis.com/fcm/send/cdEfyFDFZTA:APA91bGmRouDukhAQRHTcR0fsdSzE05_cyb27u3SSmhdW1ziIAxp5gDsMWbvd8EB4QPHBMrQZ9EWsjv2rLDVfaM_t12OsJgRP2gr8zUcvOWNcNgd5IUC3aXh2ZQEX7GcnabN5iZHoRJx",
-    expirationTime:null,
-    keys:{
-        p256dh:"BCDZ4uVJf5oMoHGiPLigYPsUW4Vxoc73ry4uEGL2HynU-JObih8ZcwCGtVmrVNs3XBb7bp-8zsoSDcUAeDakYBw",
-          auth:"K1cQMNMSQWUXydI1hl8w2A"
-    }}
+let sub = {}
 
 push.sendNotification(sub, 'test message');
